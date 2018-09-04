c77ef08820384a579f14dfac70c27c7ebe06c39a
t.s.e.a.r <t.s.e.a.r@outlook.fr>
2018-09-04 19:12:35 +0200

    no message

--- helloState.js
+++ helloState.js
@@ -11,7 +11,7 @@
             response[0] = {text:'Welcome to OLE Bank'};
             console.log(state.senderPsid);
 
-            if(state.senderPsid ===902533626537343){
+            if(state.senderPsid === '902533626537343'){
                 response[1] = {text:'We have identified that your Facebook account is associated a main account number ending with 5555. Would you like to continue with this account number?'};
             }else{
                 response[1] ={text:'Do you have a bank account number at OLE Bank?'}; 
