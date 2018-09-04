b407616ccef5bb1381233ab5270e8fa8ff1f81b7
t.s.e.a.r <t.s.e.a.r@outlook.fr>
2018-09-04 18:56:01 +0200

    initiating the flow.

--- helloState.js
+++ helloState.js
@@ -8,12 +8,12 @@
         if(action==="DISPLAY_WELCOME_MESSAGE"){
             var response=[];
 
-            response.append({text:'Welcome to OLE Bank'});
+            response.push({text:'Welcome to OLE Bank'});
             
             if(state.senderPsid ===902533626537343){
-                response.append({text:'We have identified that your Facebook account is associated a main account number ending with 5555. Would you like to continue with this account number?'});
+                response.push({text:'We have identified that your Facebook account is associated a main account number ending with 5555. Would you like to continue with this account number?'});
             }else{
-                response.append({text:'Do you have a bank account number at OLE Bank?'}); 
+                response.push({text:'Do you have a bank account number at OLE Bank?'}); 
             }
             
             return {"state":{"state":"welcomeMessageState","senderPsid":state.senderPsid},"response":response};
