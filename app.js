f78a522351fe0d21ef209d94081e97c6955be1c9
t.s.e.a.r <t.s.e.a.r@outlook.fr>
2018-09-05 17:45:40 +0200

    no message

--- app.js
+++ app.js
@@ -85,7 +85,7 @@ function handleMessage(sender_psid, received_message) {
 
   // Handle quick_replies postbacks
 
-  if ( received_message.quick_reply.payload !== 'undefined' && received_message.quick_reply.payload ) {
+  if ( received_message.quick_reply !== 'undefined' && received_message.quick_reply ) {
     console.log("received_message.quick_reply.payload");
 
     let payload = received_message.quick_reply.payload;
