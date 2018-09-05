c0adc9766ada198bb1c9f26119567f0e16c19793
t.s.e.a.r <t.s.e.a.r@outlook.fr>
2018-09-05 22:55:51 +0200

    no message

--- app.js
+++ app.js
@@ -185,44 +185,7 @@ function handleMessage(sender_psid, received_message) {
             console.log("my sender_psid after = " + JSON.stringify(result))})
       })();
 
-      async function executeActionAgainstPayload() {
-        await keyv.get(sender_psid).then(
-          result => {
-            console.log("my sender_psid = " + sender_psid);
-            console.log("my result = " + JSON.stringify(result));
-            facebookUserState = result;
-            console.log(facebookUserState.state);
-            var currentState = stateList[facebookUserState.state];
-            console.log(currentState);
-            let currentStateResponse = currentState.executeAction(
-              payload,
-              facebookUserState
-            );
-  
-            console.log(
-              "my currentStateResponse = " + JSON.stringify(currentStateResponse)
-            );
-            console.log(
-              "my currentStateResponse.response = " +
-                JSON.stringify(currentStateResponse.response)
-            );
-  
-            // response = {text:'Welcome Mr. Tarek to ABCBank'};
-            response = currentStateResponse.response;
-            console.log("my response = " + JSON.stringify(response));
-  
-            keyv.set(sender_psid, currentStateResponse.state, 120000);
-  
-            for (const element of response) {
-              console.log(element);
-              callSendAPI(sender_psid, element);
-            }
-  
-            return response;
-          }
-          // Send the message to acknowledge the postback
-        );
-      }
+     
 
       payload = "DISPLAY_WELCOME_MESSAGE";
 
@@ -231,6 +194,45 @@ function handleMessage(sender_psid, received_message) {
       console.log("test", response);
       // await keyv.get(sender_psid).then(result =>  console.log(JSON.stringify(result)))
     });
+
+    async function executeActionAgainstPayload() {
+      await keyv.get(sender_psid).then(
+        result => {
+          console.log("my sender_psid = " + sender_psid);
+          console.log("my result = " + JSON.stringify(result));
+          facebookUserState = result;
+          console.log(facebookUserState.state);
+          var currentState = stateList[facebookUserState.state];
+          console.log(currentState);
+          let currentStateResponse = currentState.executeAction(
+            payload,
+            facebookUserState
+          );
+
+          console.log(
+            "my currentStateResponse = " + JSON.stringify(currentStateResponse)
+          );
+          console.log(
+            "my currentStateResponse.response = " +
+              JSON.stringify(currentStateResponse.response)
+          );
+
+          // response = {text:'Welcome Mr. Tarek to ABCBank'};
+          response = currentStateResponse.response;
+          console.log("my response = " + JSON.stringify(response));
+
+          keyv.set(sender_psid, currentStateResponse.state, 120000);
+
+          for (const element of response) {
+            console.log(element);
+            callSendAPI(sender_psid, element);
+          }
+
+          return response;
+        }
+        // Send the message to acknowledge the postback
+      );
+    }
 
       response = {
         text: `Your state now is set to helloState`, 
