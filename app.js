3faf1e108424033998a0d9f59beb62356aeb46ef
t.s.e.a.r <t.s.e.a.r@outlook.fr>
2018-09-05 15:26:58 +0200

    no message

--- app.js
+++ app.js
@@ -265,21 +265,18 @@ function callSendAPI(sender_psid, response) {
     },
     message: response
   };
+  var requestObject = {
+    uri: "https://graph.facebook.com/v2.6/me/messages",
+    qs: { access_token: process.env.PAGE_ACCESS_TOKEN },
+    method: "POST",
+    json: request_body
+  };
   // Send the HTTP request to the Messenger Platform
-  request(
-    {
-      uri: "https://graph.facebook.com/v2.6/me/messages",
-      qs: { access_token: process.env.PAGE_ACCESS_TOKEN },
-      method: "POST",
-      json: request_body
-    },
-    (err, res, body) => {
+  request(requestObject,(err, res, body) => {
       if (!err) {
         console.log("message sent!");
-        deferred.resolve();
       } else {
         console.error("Unable to send message:" + err);
-        deferred.reject();
       }
     }
   );
