2c928b24cd99b265aadab327d0d48f0f6ca140e5
t.s.e.a.r <t.s.e.a.r@outlook.fr>
2018-09-07 13:26:53 +0200

    no message

--- app.js
+++ app.js
@@ -226,9 +226,10 @@ function handleMessage(sender_psid, received_message) {
 }
 test();
       
+console.log("user_state",user_state);
 
       if(
-        user_state == "yumaFirstAttempt" || user_state == "yumaSecondAttempt" || user_state == "yumaThirdAttempt" && received_message.text == "123456"
+        user_state.state == "yumaFirstAttempt" || user_state.state == "yumaSecondAttempt" || user_state.state == "yumaThirdAttempt" && received_message.text == "123456"
       ){
         console.log("password entered")
             console.log("72) my sender_psid = " + sender_psid);
