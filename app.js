d962f0557a64462b83090f66b47a87d11fa510d3
t.s.e.a.r <t.s.e.a.r@outlook.fr>
2018-09-07 14:21:48 +0200

    no message

--- app.js
+++ app.js
@@ -228,9 +228,13 @@ function handleMessage(sender_psid, received_message) {
  user_state = test();
       
 console.log("user_state.state",user_state);
-user_state = user_state.then(
-  result => {return result })
-  console.log("user_state.state.last",user_state);
+user_state_1 = user_state.then(
+  result => {
+    console.log(result)
+    return result })
+
+  
+  console.log("user_state.state.user_state_1",user_state_1);
 
 
       if(
