a232e7643eb791376c316a64c9eb29275b335125
t.s.e.a.r <t.s.e.a.r@outlook.fr>
2018-09-10 19:40:15 +0200

    no message

--- MainMenu.js
+++ MainMenu.js
@@ -2,7 +2,7 @@
 
 // var exports = module.exports = {};
 
-var reportStolenCard = {
+var MainMenu = {
   executeAction: function(action, state) {
     console.log("action", action);
     var response = [];
@@ -10,32 +10,6 @@ var reportStolenCard = {
     if (action === "YES_REPORT_STOLEN_CARD") {
       //
       console.log("YES_REPORT_STOLEN_CARD");
-
-      // response[0] = {
-      //   attachment: {
-      //     type: "template",
-      //     payload: {
-      //       template_type: "generic",
-      //       elements: [
-      //         {
-      //           title: "Which credit card would you like to stop",
-      //           buttons: [
-      //             {
-      //               type: "postback",
-      //               title: "234234234234234232",
-      //               payload: "CREDIT_CARD_1"
-      //             },
-      //             {
-      //               type: "postback",
-      //               title: "4534534534534534",
-      //               payload: "CREDIT_CARD_2"
-      //             }
-      //           ]
-      //         }
-      //       ]
-      //     }
-      //   }
-      // };
 
       response[0] = {
         text:
@@ -87,37 +61,6 @@ var reportStolenCard = {
           }
         ]
       };
-      // response[0] = {
-      //   attachment: {
-      //     type: "template",
-      //     payload: {
-      //       template_type: "generic",
-      //       elements: [
-      //         {
-      //           title: "Is this the right picture?",
-      //           subtitle: "Tap a button to answer.",
-      //           buttons: [
-      //             {
-      //               type: "postback",
-      //               title: "Main Account Balance",
-      //               payload: "Main_Account_Balance"
-      //             },
-      //             {
-      //               type: "postback",
-      //               title: "Main Account Transactions",
-      //               payload: "Main_Account_Transactions"
-      //             },
-      //             {
-      //               type: "postback",
-      //               title: "Cancel",
-      //               payload: "Cancel"
-      //             }
-      //           ]
-      //         }
-      //       ]
-      //     }
-      //   }
-      // };
 
       return {
         state: { state: "MainMenu", senderPsid: state.senderPsid },
@@ -127,4 +70,53 @@ var reportStolenCard = {
   }
 };
 
-module.exports = reportStolenCard;
+module.exports = MainMenu;
+
+
+"use strict";
+
+
+// var exports = module.exports = {};
+
+ var MainMenu =  {
+    executeAction:function(state){
+        console.log("chooseAccountState function:");
+        var response=[];
+// there is no actions in this state
+        // if(action==="YES_USE_MAIN_ACCOUNT"){
+            response[0] = {
+                text:
+                  "We have identified that your Facebook account is associated a main account number ending with 5555. Would you like to continue with this account number?",
+                quick_replies: [
+                  {
+                    content_type: "text",
+                    title: "Main Account Balance",
+                    payload: "Main_Account_Balance"
+                  },
+                  {
+                    content_type: "text",
+                    title: "Main Account Transactions",
+                    payload: "Main_Account_Transactions"
+                  },
+                  {
+                    content_type: "text",
+                    title: "E-Payments",
+                    payload: "Main_Account_Transactions"
+                  },
+                  {
+                    content_type: "text",
+                    title: "Cancel",
+                    payload: "E-Payments"
+                  }
+                ]
+              };
+            //i should execute another action that move the user to entering first attampt state
+            // next state Submit ticket with issue will contain buttons that has the credit numbers of the user and a postback called STOLEN_CREDIT_CARD_TO_BE_REPORTED
+            return {"state":{"state":"waitState","senderPsid":state.senderPsid},"response":response};
+         
+    }
+};
+
+module.exports = MainMenu;
+
+
