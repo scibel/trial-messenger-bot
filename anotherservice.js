e70ed7a4b360605df77de417652a033692699a37
Alaa Nabil <alnabil.ext@orange.com>
2018-09-11 01:19:17 +0200

    Fix issue

--- anotherservice.js
+++ anotherservice.js
@@ -3,12 +3,12 @@
 var anotherService = {
     executeAction: function (action, state) {
         var response = [];
-        if (action === "NO") {
+        if (action === "WOULD_YOU_LIKE_ANOTHER_SERVICE_NO") {
             response[0] = { text: 'Thank you for chatting with OLE Bank Bot. For more information, please access our website @ www.olebank.com or contact us @ 19555' };
-            response[1] = { text: 'IF you would like to re-engage with the bot please say Hi' }
+            response[1] = { text: 'If you would like to re-engage with the bot please say Hi' }
             return { "state": { "state": "thankYouState", "senderPsid": state.senderPsid }, "response": response };
         }
-        else if (action === "YES") {
+        else if (action === "WOULD_YOU_LIKE_ANOTHER_SERVICE_YES") {
             response[0] = {
                 text: "Please, choose one of the services below: \n"
                     + "(1) Main Account Balance \n"
