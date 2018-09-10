ae8a8652dcb4e8b16cf5a9712b33bff3a6015887
Alaa Nabil <alnabil.ext@orange.com>
2018-09-11 01:23:18 +0200

    Quick fix

--- stolenCardTicket.js
+++ stolenCardTicket.js
@@ -14,6 +14,22 @@
             response[0] = {text:'Your report has been submitted. You will be contact shortly by a customer service representative. Would you like another service?'};
         }
 
+        response[1] = {
+            text: 'Would you like another service?',
+            quick_replies: [
+                {
+                    content_type: "text",
+                    title: "Yes",
+                    payload: "WOULD_YOU_LIKE_ANOTHER_SERVICE_YES"
+                },
+                {
+                    content_type: "text",
+                    title: "No",
+                    payload: "WOULD_YOU_LIKE_ANOTHER_SERVICE_NO"
+                }
+            ]
+        };
+
         return {"state":{"state":"anotherService","senderPsid":state.senderPsid},"response":response};
     }
 };
