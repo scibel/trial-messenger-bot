bed23408a708adc338054026c8e3c276298fe557
Alaa Nabil <alnabil.ext@orange.com>
2018-09-11 02:07:03 +0200

    no message

--- reportStolenCard.js
+++ reportStolenCard.js
@@ -44,44 +44,37 @@ var reportStolenCard = {
 
     } else if (action === "NO_I_DO_NOT_REPORT_STOLEN_CARD") {
       response[0] = {
-        text: "Please, choose one of the services below:",
         attachment: {
-          type: "template",
-          payload: {
-            template_type: "generic",
-            elements: [
-              {
-                title: "Please choose card/key that has been lost/stolen: ",
-                buttons: [
-                  {
-                    type: "postback",
-                    title: "Balance",
-                    payload: "ACCOUNT_BALANCE"
-                  },
-                  {
-                    type: "postback",
-                    title: "Transactions",
-                    payload: "ACCOUNT_TRANSACTIONS"
-                  },
-                  {
-                    type: "postback",
-                    title: "E-Payment",
-                    payload: "PAYMENT"
-                  },
-                  {
-                    type: "postback",
-                    title: "Cancel and Logout",
-                    payload: "CANCEL"
-                  }
+            type: "template",
+            payload: {
+                template_type: "generic",
+                elements: [
+                    {
+                        title: "Please, choose one of the services below:",
+                        buttons: [
+                            {
+                                type: "postback",
+                                title: "Account Balance",
+                                payload: "ACCOUNT_BALANCE"
+                            },
+                            {
+                                type: "postback",
+                                title: "Account Transactions",
+                                payload: "ACCOUNT_TRANSACTIONS"
+                            },
+                            {
+                                type: "postback",
+                                title: "Cancel and Logout",
+                                payload: "LOGOUT"
+                            }
+                        ]
+                    }
                 ]
-              }
-            ]
-          }
+            }
         }
 
-      };
-      return { "state": { "state": "ACCOUNT_SERVICE_STATE", "senderPsid": state.senderPsid }, "response": response };
-
+    }
+    return { "state": { "state": "accountServiceState", "senderPsid": state.senderPsid }, "response": response };
     }
   }
 };
