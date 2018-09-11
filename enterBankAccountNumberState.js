5554c25f3074603df9196bc628dbd111e1cd5490
Alaa Nabil <alnabil.ext@orange.com>
2018-09-11 16:14:15 +0200

    Handle wrong account number

--- enterBankAccountNumberState.js
+++ enterBankAccountNumberState.js
@@ -4,11 +4,19 @@
     executeAction:function(action,state){
         var response=[];
 
-        response[0] = {text: "You have entered a correct account number"};
+        if(action === "1234567890"){
+            response[0] = {text: "You have entered a correct account number"};
 
-        response[1] = {text:'Enter your pin code'};
+            response[1] = {text:'Enter your pin code'};
+            
+            return {"state":{"state":"yumaSecondAttempt","senderPsid":state.senderPsid},"response":response};
+        } else{
+            response[0] = {text: "We have not identified your account number."};
+            response[1] = {text:'Please enter your bank account number'};
+            return {"state":{"state":"enterBankAccountNumberState","senderPsid":state.senderPsid},"response":response};
+        }
+
         
-        return {"state":{"state":"yumaSecondAttempt","senderPsid":state.senderPsid},"response":response};
         
     }
 };
